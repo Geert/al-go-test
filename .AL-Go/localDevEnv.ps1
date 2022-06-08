fb425187309bfcb3ea5a62882f7a3b1acc1407bf
Geert Rozendom <g.rozendom@newminds.nl>
2022-06-08 12:18:56 +0200

    Goed man

--- .AL-Go/localDevEnv.ps1
+++ .AL-Go/localDevEnv.ps1
@@ -39,18 +39,18 @@ if ($pshost.Name -eq "Visual Studio Code Host") {
 }
 
 try {
-$ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
-$webClient = New-Object System.Net.WebClient
-$webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumentList ([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore)
-$webClient.Encoding = [System.Text.Encoding]::UTF8
-Write-Host "Downloading AL-Go Helper script"
-$webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v1.5/AL-Go-Helper.ps1', $ALGoHelperPath)
-. $ALGoHelperPath -local
+    $ALGoHelperPath = "$([System.IO.Path]::GetTempFileName()).ps1"
+    $webClient = New-Object System.Net.WebClient
+    $webClient.CachePolicy = New-Object System.Net.Cache.RequestCachePolicy -argumentList ([System.Net.Cache.RequestCacheLevel]::NoCacheNoStore)
+    $webClient.Encoding = [System.Text.Encoding]::UTF8
+    Write-Host "Downloading AL-Go Helper script"
+    $webClient.DownloadFile('https://raw.githubusercontent.com/microsoft/AL-Go-Actions/v1.5/AL-Go-Helper.ps1', $ALGoHelperPath)
+    . $ALGoHelperPath -local
 
-$baseFolder = Join-Path $PSScriptRoot ".." -Resolve
+    $baseFolder = Join-Path $PSScriptRoot ".." -Resolve
 
-Clear-Host
-Write-Host -ForegroundColor Yellow @'
+    Clear-Host
+    Write-Host -ForegroundColor Yellow @'
   _                     _   _____             ______            
  | |                   | | |  __ \           |  ____|           
  | |     ___   ___ __ _| | | |  | | _____   __ |__   _ ____   __
@@ -60,7 +60,7 @@ Write-Host -ForegroundColor Yellow @'
                                                                 
 '@
 
-Write-Host @'
+    Write-Host @'
 This script will create a docker based local development environment for your project.
 
 NOTE: You need to have Docker installed, configured and be able to create Business Central containers for this to work.
@@ -71,81 +71,82 @@ The script will also modify launch.json to have a Local Sandbox configuration po
 
 '@
 
-$settings = ReadSettings -baseFolder $baseFolder -userName $env:USERNAME
+    $settings = ReadSettings -baseFolder $baseFolder -userName $env:USERNAME
 
-Write-Host "Checking System Requirements"
-$dockerService = Get-Service docker -ErrorAction SilentlyContinue
-if (-not $dockerService -or $dockerService.Status -ne "Running") {
-    throw "Creating a local development enviroment requires you to have Docker installed and running."
-}
-
-if ($settings.keyVaultName) {
-    if (-not (Get-Module -ListAvailable -Name 'Az.KeyVault')) {
-        throw "A keyvault name is defined in Settings, you need to have the Az.KeyVault PowerShell module installed (use Install-Module az) or you can set the keyVaultName to an empty string in the user settings file ($($ENV:UserName).Settings.json)."
+    Write-Host "Checking System Requirements"
+    $dockerService = Get-Service docker -ErrorAction SilentlyContinue
+    if (-not $dockerService -or $dockerService.Status -ne "Running") {
+        throw "Creating a local development enviroment requires you to have Docker installed and running."
     }
-}
 
-Write-Host
-
-if (-not $containerName) {
-    $containerName = Enter-Value `
-        -title "Container name" `
-        -question "Please enter the name of the container to create" `
-        -default "bcserver"
-}
-
-if (-not $auth) {
-    $auth = Select-Value `
-        -title "Authentication mechanism for container" `
-        -options @{ "Windows" = "Windows Authentication"; "UserPassword" = "Username/Password authentication" } `
-        -question "Select authentication mechanism for container" `
-        -default "UserPassword"
-}
-
-if (-not $credential) {
-    if ($auth -eq "Windows") {
-        $credential = Get-Credential -Message "Please enter your Windows Credentials" -UserName $env:USERNAME
-        $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
-        $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain,$credential.UserName,$credential.GetNetworkCredential().password)
-        if ($null -eq $domain.name) {
-            Write-Host -ForegroundColor Red "Unable to verify your Windows Credentials, you might not be able to authenticate to your container"
+    if ($settings.keyVaultName) {
+        if (-not (Get-Module -ListAvailable -Name 'Az.KeyVault')) {
+            throw "A keyvault name is defined in Settings, you need to have the Az.KeyVault PowerShell module installed (use Install-Module az) or you can set the keyVaultName to an empty string in the user settings file ($($ENV:UserName).Settings.json)."
         }
     }
-    else {
-        $credential = Get-Credential -Message "Please enter username and password for your container" -UserName "admin"
-    }
-}
 
-if (-not $licenseFileUrl) {
-    if ($settings.type -eq "AppSource App") {
-        $description = "When developing AppSource Apps, your local development environment needs the developer licensefile with permissions to your AppSource app object IDs"
-        $default = ""
-    }
-    else {
-        $description = "When developing PTEs, you can optionally specify a developer licensefile with permissions to object IDs of your dependant apps"
-        $default = "none"
+    Write-Host
+
+    if (-not $containerName) {
+        $containerName = Enter-Value `
+            -title "Container name" `
+            -question "Please enter the name of the container to create" `
+            -default "bcserver"
     }
 
-    $licenseFileUrl = Enter-Value `
-        -title "LicenseFileUrl" `
-        -description $description `
-        -question "Local path or a secure download URL to license file " `
-        -default $default
-
-    if ($licenseFileUrl -eq "none") {
-        $licenseFileUrl = ""
+    if (-not $auth) {
+        $auth = Select-Value `
+            -title "Authentication mechanism for container" `
+            -options @{ "Windows" = "Windows Authentication"; "UserPassword" = "Username/Password authentication" } `
+            -question "Select authentication mechanism for container" `
+            -default "UserPassword"
     }
-}
 
-CreateDevEnv `
-    -kind local `
-    -caller local `
-    -containerName $containerName `
-    -baseFolder $baseFolder `
-    -auth $auth `
-    -credential $credential `
-    -LicenseFileUrl $licenseFileUrl `
-    -InsiderSasToken $insiderSasToken
+    if (-not $credential) {
+        if ($auth -eq "Windows") {
+            $credential = Get-Credential -Message "Please enter your Windows Credentials" -UserName $env:USERNAME
+            $CurrentDomain = "LDAP://" + ([ADSI]"").distinguishedName
+            $domain = New-Object System.DirectoryServices.DirectoryEntry($CurrentDomain, $credential.UserName, $credential.GetNetworkCredential().password)
+            if ($null -eq $domain.name) {
+                Write-Host -ForegroundColor Red "Unable to verify your Windows Credentials, you might not be able to authenticate to your container"
+            }
+        }
+        else {
+            $credential = Get-Credential -Message "Please enter username and password for your container" -UserName "admin"
+        }
+    }
+
+    if (-not $licenseFileUrl) {
+        if ($settings.type -eq "AppSource App") {
+            $description = "When developing AppSource Apps, your local development environment needs the developer licensefile with permissions to your AppSource app object IDs"
+            $default = ""
+        }
+        else {
+            $description = "When developing PTEs, you can optionally specify a developer licensefile with permissions to object IDs of your dependant apps"
+            $default = "none"
+        }
+
+        $licenseFileUrl = Enter-Value `
+            -title "LicenseFileUrl" `
+            -description $description `
+            -question "Local path or a secure download URL to license file " `
+            -default $default
+
+        if ($licenseFileUrl -eq "none") {
+            $licenseFileUrl = ""
+        }
+    }
+
+    CreateDevEnv `
+        -kind local `
+        -caller local `
+        -containerName $containerName `
+        -baseFolder $baseFolder `
+        -auth $auth `
+        -credential $credential `
+        -LicenseFileUrl $licenseFileUrl `
+        -InsiderSasToken $insiderSasToken `
+        -keepContainer
 }
 finally {
     if ($fromVSCode) {
